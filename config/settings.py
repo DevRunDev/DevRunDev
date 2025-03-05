605d392611efcd68e644b7d1296bb5b7f34462dd
kangyeon <rkddus0517@naver.com>
2025-03-05 17:26:09 +0900

    ✅ Add google social login

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
     "debug_toolbar",
     "django_extensions",
 ]
