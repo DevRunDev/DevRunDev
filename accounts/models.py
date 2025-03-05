f1a2357e926ea04e7809bc2d5812419e3a17e0f9
jake baek <jake.baek@gmail.com>
2025-03-05 15:33:52 +0900

    ✅ black .

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     is_verified = models.BooleanField(default=True)
-    ROLE_CHOICES = [
-        ('Student', '학생'),
-        ('Teacher', '강사'),
-        ('manager', '관리자')
-    ]
+    ROLE_CHOICES = [("Student", "학생"), ("Teacher", "강사"), ("manager", "관리자")]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
