412e0c1054b4c72a5e8665c8088cec13222ea893
csy <choseyong0213@gmail.com>
2025-03-06 14:11:18 +0900

    🔧 fix: apply pre-commit auto-fixes (black, end-of-file-fixer)

--- courses/migrations/0002_section.py
+++ courses/migrations/0002_section.py
@@ -7,17 +7,32 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('courses', '0001_initial'),
+        ("courses", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Section',
+            name="Section",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('order', models.PositiveIntegerField()),
-                ('course', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='sections', to='courses.course')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("order", models.PositiveIntegerField()),
+                (
+                    "course",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="sections",
+                        to="courses.course",
+                    ),
+                ),
             ],
         ),
     ]
