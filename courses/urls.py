235127ae63a1c1f29d98430a7464553c6e46fcee
csy <choseyong0213@gmail.com>
2025-03-07 17:47:36 +0900

    ✅ feat: CourseCreateView 구현 및 템플릿 추가

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CourseDetailView, CourseListView
+from .views import CourseCreateView, CourseDetailView, CourseListView
 
 urlpatterns = [
     path("", CourseListView.as_view(), name="course_list"),
     path("<int:pk>/", CourseDetailView.as_view(), name="course_detail"),
+    path("create/", CourseCreateView.as_view(), name="course_create"),
 ]
